6cebbd10e172aee1d0e57b87ff625e1e57a67eb9
Minecat <120320914+Minecat300@users.noreply.github.com>
2025-11-11 12:45:31 +0100

    test

---
+++ main.js
@@ -0,0 +1,64 @@
+import fs from "fs";
+import path from "path";
+import minimist from "minimist";
+import chalk from "chalk";
+import ora from "ora";
+
+chalk.orange = chalk.rgb(255, 81, 0)
+chalk.trueCyan = chalk.rgb(39, 185, 232);
+
+function addAutoUpdateFromPackage(pkg) {
+    try {
+        if (!pkg.autoUpdate) throw chalk.orange("Package missing auto update");
+        if (!pkg.autoUpdate.fromVersion || !pkg.autoUpdate.githubWebhook) throw chalk.orange("Package missing version or webhook update");
+
+        if (pkg.autoUpdate.fromVersion) {
+            
+        }
+        if (pkg.autoUpdate.githubWebhook) {
+
+        }
+    } catch (err) {
+        throw(err);
+    }
+}
+
+function addFromPath(dir) {
+    try {
+        if (!dir) throw chalk.orange("Path missing");
+
+        const packagePath = path.join(dir, "package.json");
+        if (!fs.existsSync(packagePath)) throw chalk.orange("package.json was not found");
+        
+        const pkg = JSON.parse(fs.readFileSync(packagePath));
+        addAutoUpdateFromPackage(pkg);
+
+    } catch (err) {
+        throw(err);
+    }
+}
+
+function main() {
+    try {
+        const args = minimist(process.argv.slice(2), {
+            boolean: []
+        });
+
+        const command = args._[0];
+
+        if (command == "add") {
+            addFromPath(args._[1]);
+            return;
+        }
+        if (command == "help" || command == "h" || command == "?" || !command) {
+            console.log(chalk.trueCyan("add"), "<Package Path>");
+            return;
+        }
+        console.log(chalk.trueCyan("Unknown Command"));
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+main();
